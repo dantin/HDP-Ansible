69da0b74445c4c4071fe71b101c934089b98e491
dantin <chengjie.ding@gmail.com>
2018-12-29 16:57:44 +0800

    fix.

--- set_cloud.sh
+++ set_cloud.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
 if [ -z "$CLOUD_TO_USE" ]; then
-    echo "CLOUD_TO_USE environment variable must be set to one of the following: static"
+    echo "CLOUD_TO_USE environment variable must be set to one of the following: heurilyze"
     exit 1
 fi
 
 cloud_to_use=$(echo "$CLOUD_TO_USE" | tr '[:upper:]' '[:lower:]')
 case $cloud_to_use in
-static)
-    message="The static inventory will be used."
+heurilyze)
+    message="The heurilyze inventory will be used."
     ;;
 *)
-    message="CLOUD_TO_USE environment variable was set to \"$CLOUD_TO_USE\" but must be set to one of the following: static"
+    message="CLOUD_TO_USE environment variable was set to \"$CLOUD_TO_USE\" but must be set to one of the following: heurilyze"
     echo -e $message
     exit 1
     ;;
